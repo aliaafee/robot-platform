9a6f6d9ece2d97fc08fbfd94bc1408f78c49dd82
Ali Aafee <ali.aafee@gmail.com>
2021-10-15 11:53:54 +0545

    Organize

---
+++ Control.py/__main__.py
@@ -0,0 +1,11 @@
+"""
+Control.py
+----------
+
+Control for robot with python.
+To be run from raspberrypi zero w on board.
+Connect to driver with i2c.
+"""
+
+if __name__ == '__main__':
+    print("Hello")
